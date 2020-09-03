e6e1a26a1b1c699523cd7a84b0d92d1be584e87f
Jacob Eckert <jweckert17@gmail.com>
2020-09-03 14:54:17 -0500

    implemented routing with react router for basic page views

---
+++ src/components/nav/NavBar.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import './NavBar.css';
+
+export const NavBar = props => (
+  <ul className="navbar">
+    <li className="navbar__item">
+      <Link className="navbar__link" to="/">NSS Kennels</Link>
+    </li>
+    <li className="navbar__item">
+      <Link className="navbar__link" to="/animals">Animals</Link>
+    </li>
+    <li className="navbar__item">
+      <Link className="navbar__link" to="/customers">Customers</Link>
+    </li>
+    <li className="navbar__item">
+      <Link className="navbar__link" to="/employees">Employees</Link>
+    </li>
+  </ul>
+);
--- src/index.js
+++ src/index.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter } from 'react-router-dom';
+
 import { Kennel } from './components/Kennel';
 
 ReactDOM.render(
   <React.StrictMode>
-    <Kennel />
+    <BrowserRouter>
+      <Kennel />
+    </BrowserRouter>
   </React.StrictMode>,
   document.getElementById('root')
 );
