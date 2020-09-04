39fad5945e60dc826cd6b0aa202835c8d216cb5e
Jacob Eckert <jweckert17@gmail.com>
2020-09-04 10:40:03 -0500

    adding employee creation link, styling as button

--- src/components/employee/EmployeeList.js
+++ src/components/employee/EmployeeList.js
@@ -1,4 +1,6 @@
 import React, { useContext, useEffect } from 'react';
+import { Link } from 'react-router-dom';
+
 import { EmployeeContext } from './EmployeeProvider';
 import { Employee } from './Employee';
 
@@ -10,8 +12,12 @@ export const EmployeeList = props => {
   }, []);
 
   return (
-    <section className="list employees">
-      { employees.map(e => <Employee key={e.id} employee={e} />) }
-    </section>
+    <div className="employeesContainer">
+      <h2 className="pageHeader">Employees</h2>
+      <Link className="btn btn--create employeeFormLink" to="/employees/create">Add Employee</Link>
+      <section className="list employees">
+        { employees.map(e => <Employee key={e.id} employee={e} />) }
+      </section>
+    </div>
   );
 };
