ddf631309eb49cfb93f8bbf1233589513268f1b3
Jacob Eckert <jweckert17@gmail.com>
2020-09-04 11:47:48 -0500

    rendering location name in employee list

--- src/components/employee/Employee.js
+++ src/components/employee/Employee.js
@@ -1,13 +1,11 @@
 import React from 'react';
 import './Employee.css';
 
-export const Employee = props => {
-  const { name, locationId } = props.employee;
-
+export const Employee = ({ employee, location }) => {
   return (
     <section className="card employee">
-      <h3 className="card__header employee__name">{name}</h3>
-      <p className="card__info employee__workplace">{locationId}</p>
+      <h3 className="card__header employee__name">{employee.name}</h3>
+      <p className="card__info employee__workplace">{location.name}</p>
     </section>
   );
 };
--- src/components/employee/EmployeeList.js
+++ src/components/employee/EmployeeList.js
@@ -2,13 +2,16 @@ import React, { useContext, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 
 import { EmployeeContext } from './EmployeeProvider';
+import { LocationContext } from '../location/LocationProvider';
 import { Employee } from './Employee';
 
 export const EmployeeList = props => {
   const { employees, getEmployees } = useContext(EmployeeContext);
+  const { locations, getLocations } = useContext(LocationContext);
 
   useEffect(() => {
     getEmployees();
+    getLocations();
   }, []);
 
   return (
@@ -16,7 +19,12 @@ export const EmployeeList = props => {
       <h2 className="pageHeader">Employees</h2>
       <Link className="btn btn--create employeeFormLink" to="/employees/create">Add Employee</Link>
       <section className="list employees">
-        { employees.map(e => <Employee key={e.id} employee={e} />) }
+        { 
+          employees.map(e => {
+            const location = locations.find(l => l.id === e.locationId) || {};
+            return <Employee key={e.id} employee={e} location={location} /> 
+          })
+        }
       </section>
     </div>
   );
