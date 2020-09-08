5e50002011a0323fca24462acb8b83980a265e7b
Jacob Eckert <jweckert17@gmail.com>
2020-09-08 11:17:19 -0500

    implemented routing to animal form

---
+++ src/components/animal/AnimalForm.js
@@ -0,0 +1,11 @@
+import React, { useContext } from 'react';
+
+import { AnimalContext } from './AnimalProvider';
+import { CustomerContext } from '../customer/CustomerProvider';
+import { LocationContext } from '../location/LocationProvider';
+
+export const AnimalForm = props => {
+  return (
+    <div>hi</div>
+  );
+};
