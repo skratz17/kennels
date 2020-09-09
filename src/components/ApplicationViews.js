e9c92e50f03b986dfd91675f0753c6e8dc00f0ff
Jacob Eckert <jweckert17@gmail.com>
2020-09-09 13:51:40 -0500

    implemented animal search

--- src/components/ApplicationViews.js
+++ src/components/ApplicationViews.js
@@ -14,6 +14,7 @@ import { EmployeeForm } from './employee/EmployeeForm';
 import { EmployeeDetail } from './employee/EmployeeDetail';
 import { LocationDetail } from './location/LocationDetail';
 import { AnimalDetail } from './animal/AnimalDetail';
+import { AnimalSearch } from './animal/AnimalSearch';
 
 export const ApplicationViews = props => (
   <>
@@ -38,7 +39,10 @@ export const ApplicationViews = props => (
       <LocationProvider>
         <CustomerProvider>
           <Route exact path="/animals">
-            <AnimalList />
+            <>
+              <AnimalSearch />
+              <AnimalList />
+            </>
           </Route>
 
           <Route path="/animals/create" render={props => <AnimalForm {...props} />} />
--- src/components/animal/AnimalList.js
+++ src/components/animal/AnimalList.js
@@ -1,4 +1,4 @@
-import React, { useContext, useEffect } from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 
 import { AnimalContext } from './AnimalProvider';
@@ -7,9 +7,11 @@ import { LocationContext } from '../location/LocationProvider';
 import { Animal } from './Animal';
 
 export const AnimalList = props => {
-  const { animals, getAnimals } = useContext(AnimalContext);
+  const { animals, getAnimals, searchTerm } = useContext(AnimalContext);
   const { customers, getCustomers } = useContext(CustomerContext);
   const { locations, getLocations } = useContext(LocationContext);
+
+  const [ filteredAnimals, setFilteredAnimals ] = useState([]);
 
   useEffect(() => {
     getAnimals();
@@ -17,12 +19,22 @@ export const AnimalList = props => {
     getLocations();
   }, []);
 
+  useEffect(() => {
+    if(searchTerm === '') {
+      setFilteredAnimals(animals);
+    }
+    else {
+      const _filteredAnimals = animals.filter(a => a.name.toUpperCase().includes(searchTerm.toUpperCase()));
+      setFilteredAnimals(_filteredAnimals);
+    }
+  }, [ animals, searchTerm ]);
+
   return (
     <div className="animalsContainer">
       <h2 className="pageHeader">Animals</h2>
       <Link className="btn btn--create animalFormLink" to="/animals/create">Add Animal</Link>
       <section className="list animals">
-        { animals.map(a => {
+        { filteredAnimals.map(a => {
             const customer = customers.find(c => c.id === a.customerId) || {};
             const location = locations.find(l => l.id === a.locationId) || {};
 
